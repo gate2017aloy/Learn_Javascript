c8956c27128d7956d94b8224fcd1302af9a43c11
Aloy Kumar Das <aloydas@cisco.com>
2021-07-23 12:48:28 +0530

    Initial commit

---
+++ source_code/generators/generators.js
@@ -0,0 +1,13 @@
+// function * generateDemo() {
+//     console.log("Started Generating")
+//     console.log("Will stop now")
+// }
+//
+// const it - generateDemo();
+//
+// for(const val of it)
+//     console.log(val)
+
+
+const arr = [1,2,3.4]
+console.log(arr);
